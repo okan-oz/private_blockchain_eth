c819d6f7edc61cf971011c821a888e0bd33b866d
Okan Öz <okan.oz@outlook.com>
2024-08-20 15:35:35 +0300

    initial commit

---
+++ PrivateBlockchainNethereum/PrivateBlockchainNethereum/Controllers/AccountBalanceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrivateBlockchainNethereum.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountBalanceController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public AccountBalanceController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetAccountBalanceAsync")]
+    public  string Get()
+    {
+        return "0";
+    }
+
+}
+
---
+++ PrivateBlockchainNethereum/WrapNethereum/WrapNethereumCore.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.NetworkInformation;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3;
+
+namespace WrapNethereum;
+public class WrapNethereumCore
+{
+    private  string rpcUrl = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID";
+    private readonly Web3 web3;
+
+    public WrapNethereumCore(string rpcUrl)
+    {
+        this.rpcUrl = rpcUrl;
+        this.web3 = new Web3(rpcUrl);
+
+    }
+
+    public async Task<HexBigInteger> GetAccountBalanceAsync(string accountAddress)
+    {
+        // Hesap bakiyesini Wei cinsinden alıyoruz
+        HexBigInteger balanceInWei = await web3.Eth.GetBalance.SendRequestAsync(accountAddress);
+
+        // Bakiye, BigInteger türünde döndürülecek
+        return balanceInWei;
+    }
+        
+
+
+}
+
